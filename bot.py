c9f8e48eec4059edf3bb9881f07d6c2abf342bad
AlexNash96 <alexnash96@gmail.com>
2024-02-27 09:47:39 -0600

    Added comments and removed old commented code.

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ logger.setLevel(logging.WARNING)  # change "WARNING" to "DEBUG" for verbose logs
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-# bot stuffs
+# variables used for bot authentication and base URLs
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
@@ -24,7 +24,8 @@ BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat="
 BASE_GEO_URL = "http://api.openweathermap.org/geo/1.0/zip?"
 API_KEY = os.getenv('API_KEY')
 
-
+# The geo_code funciton is used to translate postal code location data to coordinates
+# The weather API requires coordinates to identify the location of the requested weather report
 def geo_code(zip_code, country_code):
     base_geo_url = "http://api.openweathermap.org/geo/1.0/zip?"
     geo_url = f'{base_geo_url}zip={zip_code},{country_code}&appid={API_KEY}'
@@ -42,15 +43,13 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=''))
 
 
+'''
+The !weather command is accepts a postal code and a country code which is
+sent to the geo_code function then used to get weather and air quality data
+for the specified location.
+'''
 @bot.command(pass_context = True)
 async def weather(weather, zip_code, country_code='us'):
-    # updating the URL
-    # geo_url = f'''{BASE_GEO_URL}zip={zip_code},{country_code}&appid={API_KEY}'''
-    # response = requests.get(geo_url)
-    # location = response.json()
-    # convert lat and lon to string
-    # lat = str(location['lat'])
-    # lon = str(location['lon'])
 
     coords = geo_code(zip_code, country_code)
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={coords[0]}&lon={coords[1]}&appid={API_KEY}'
@@ -66,7 +65,7 @@ async def weather(weather, zip_code, country_code='us'):
         air_data = air_response.json()
         # getting the main dict block
         main = data['main']
-        # getting temperature
+        # getting temperature: temp is converted from Kelvin to Farenheit
         temperature = int((main['temp'] - 273.15) * 9/5 + 32)
         # getting the humidity
         humidity = main['humidity']
@@ -81,8 +80,17 @@ async def weather(weather, zip_code, country_code='us'):
         # await weather.author.send(output)
 
 
+
+'''
+The !myweather command is used to get a users weather data after they have saved
+their location data with the bot. The coordinates are stored to avoid unnecessary
+calls to the geocoding API.
+
+The location and user data is currently stored in a CSV
+'''
 @bot.command(pass_context = True)
 async def myweather(myweather):
+    #TO DO: update function to store in DB instead of CSV
     with open(os.path.join(os.getcwd(), "User_locations.csv"),"r+",encoding='UTF8',newline='') as input_file:
         csv_rows = csv.DictReader(input_file)
         for line in csv_rows:
@@ -124,13 +132,22 @@ async def myweather(myweather):
                     await myweather.author.send(f"I do not have your location data, please send me a message with !mylocation zip_code country_code")
 
 
+'''
+The !mylocation command is used for users to save their user and location data
+to the bot. This command is best used in a direct message to avoid posting location 
+data publicly.
+
+The mylocation command will send the location data to the geo_code function to get
+coordinates which will be stored in CSV.
+'''
 @bot.command(pass_context = True)
 async def mylocation(mylocation, zip_code, country_code):
+    '''TO DO: add temperature unit as preference so users can choose to report temperature
+       in farenheit or celsius'''
     coords = geo_code(zip_code, country_code)
     user = str(mylocation.author)
     user_dict = {"username": user, "lat": coords[0], "lon": coords[1]}
     fieldnames = ['username','lat','lon']
-    #with open(f"{str(mylocation.author)}.pickle", "wb") as output_file:
     with open(os.path.join(os.getcwd(), "User_locations.csv"),"r+",encoding='UTF8',newline='') as input_file:
         csv_rows = csv.DictReader(input_file)
         for line in csv_rows:
@@ -142,12 +159,15 @@ async def mylocation(mylocation, zip_code, country_code):
                     writer.writerow(user_dict)
                     #cPickle.dump(user_dict, output_file)
                     await mylocation.author.send(f"your location is saved as user = {mylocation.author} lat = {coords[0]} and lon = {coords[1]}")
-    # may be best to build database to store user data instead of pickle files
+    
 
-
+'''
+The !testsave command is used for testing purposes to ensure user data is saved
+successfully. The command simply looks for saved user data and sends a message
+with the username and saved coordinates.
+'''
 @bot.command(pass_context = True)
 async def testsave(testsave):
-    #with open(f"{str(testsave.author)}.pickle", "rb") as input_file:
     with open(os.path.join(os.getcwd(), "User_locations.csv"),"r+",encoding='UTF8',newline='') as input_file:
         csv_rows = csv.DictReader(input_file)
         for line in csv_rows:
@@ -158,13 +178,11 @@ async def testsave(testsave):
                         "lon": line['lon']
                     }
 
-        #user_dict = cPickle.load(input_file)
-
     await testsave.author.send(f"your location is saved as user = {user_dict['user']}\nlat = {user_dict['lat']}\nlon = {user_dict['lon']}")
 '''
 @bot.command(pass_context = True)
 async def f10(f10, zip_code, country_code):
-    # command for forecast
+    # command for 10-day forecast
 
 @bot.command(pass_context = True)
 async def radar(radar, zip_code, country_code):
